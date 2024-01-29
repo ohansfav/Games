4ac8bd70c3f07923a1b6cf32dd0a59a4054f9ef3
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-01-29 22:05:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is my guess game Python script.
+# This is my guess game Python prog script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
